3a7ccdc81c77b79f3a850ce59a985d78da67d13f
matkrin <matkrin@protonmail.com>
2024-02-07 06:36:40 +0100

    feat: Better diagnostic messages for broken LinkRef and FootnoteRef

--- src/diagnostics.rs
+++ src/diagnostics.rs
@@ -60,10 +60,7 @@ fn handle_broken_link(state: &State, link: &Link) -> Vec<BrokenLink> {
             if let Some(pos) = &link.position {
                 let range = range_from_position(pos);
                 let message = format!("Link to non-existent file `{}`", link.url);
-                broken_links.push(BrokenLink {
-                    range,
-                    message,
-                })
+                broken_links.push(BrokenLink { range, message })
             };
         }
     };
@@ -77,9 +74,12 @@ fn handle_broken_ref(req_uri: &Url, state: &State) -> Vec<BrokenLink> {
     find_broken_link_ref(ast, &re, &mut ranges);
     ranges
         .iter()
-        .map(|r| BrokenLink {
-            range: *r,
-            message: "Link reference to non-existent link definition".to_string(),
+        .map(|broken_link_ref| BrokenLink {
+            range: broken_link_ref.range,
+            message: format!(
+                "Link reference to non-existent link definition `{}`",
+                broken_link_ref.text
+            ),
         })
         .collect::<Vec<_>>()
 }
@@ -91,14 +91,22 @@ fn handle_broken_footnote_ref(req_uri: &Url, state: &State) -> Vec<BrokenLink> {
     find_broken_link_ref(ast, &re, &mut ranges);
     ranges
         .iter()
-        .map(|r| BrokenLink {
-            range: *r,
-            message: "Footnote reference to non-existent footnote definition".to_string(),
+        .map(|broken_link_ref| BrokenLink {
+            range: broken_link_ref.range,
+            message: format!(
+                "Footnote reference to non-existent footnote definition `{}`",
+                broken_link_ref.text
+            ),
         })
         .collect()
 }
 
-fn find_broken_link_ref(node: &Node, re: &Regex, positions: &mut Vec<Range>) {
+struct BrokenLinkRef<'a> {
+    range: Range,
+    text: &'a str,
+}
+
+fn find_broken_link_ref<'a>(node: &'a Node, re: &Regex, positions: &mut Vec<BrokenLinkRef<'a>>) {
     if let Some(children) = node.children() {
         for child in children {
             find_broken_link_ref(child, re, positions)
@@ -110,16 +118,23 @@ fn find_broken_link_ref(node: &Node, re: &Regex, positions: &mut Vec<Range>) {
             for captures in re.captures_iter(line) {
                 let start = captures.get(0).map(|m| m.start());
                 let end = captures.get(0).map(|m| m.end());
-                if let (Some(s), Some(e), Some(pos)) = (start, end, &text.position) {
-                    positions.push(Range {
-                        start: Position {
-                            line: (pos.start.line + i - 1) as u32,
-                            character: (pos.start.column + s - 1) as u32,
+                let broken_link_text = captures.get(0).map(|m| m.as_str());
+
+                if let (Some(s), Some(e), Some(text), Some(pos)) =
+                    (start, end, broken_link_text, &text.position)
+                {
+                    positions.push(BrokenLinkRef {
+                        range: Range {
+                            start: Position {
+                                line: (pos.start.line + i - 1) as u32,
+                                character: (pos.start.column + s - 1) as u32,
+                            },
+                            end: Position {
+                                line: (pos.start.line + i - 1) as u32,
+                                character: (pos.start.column + e - 1) as u32,
+                            },
                         },
-                        end: Position {
-                            line: (pos.start.line + i - 1) as u32,
-                            character: (pos.start.column + e - 1) as u32,
-                        },
+                        text,
                     });
                 }
             }

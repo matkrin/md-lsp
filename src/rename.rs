f59ba346cea80c3a9c8ae3b5546c63ed70b8c645
matkrin <matkrin@protonmail.com>
2024-02-13 16:46:17 +0100

    Rename: right range for LinkReference

--- src/rename.rs
+++ src/rename.rs
@@ -1,17 +1,54 @@
-use lsp_types::{Position, PrepareRenameResponse};
+use lsp_types::{Position, PrepareRenameResponse, Range};
 use markdown::mdast::{
-    Definition, FootnoteDefinition, FootnoteReference, Heading, LinkReference, Node,
+    Definition, FootnoteDefinition, FootnoteReference, Heading, LinkReference, Node, Text,
 };
 
 use crate::traverse_ast;
 
 pub fn rename(node: &Node) -> Option<PrepareRenameResponse> {
     match node {
-        Node::Heading(Heading { position, .. })
-        | Node::LinkReference(LinkReference { position, .. })
-        | Node::Definition(Definition { position, .. })
-        | Node::FootnoteReference(FootnoteReference { position, .. })
-        | Node::FootnoteDefinition(FootnoteDefinition { position, .. }) => {
+        Node::Heading(Heading { position, .. }) => {
+            log::info!("RENAME NODE: {:?}", node);
+            return Some(PrepareRenameResponse::DefaultBehavior {
+                default_behavior: true,
+            });
+        }
+        Node::LinkReference(LinkReference {
+            position,
+            children,
+            identifier,
+            ..
+        }) => {
+            let text = get_link_ref_text(children)?;
+            if let (Some(link_ref_pos), Some(text_pos)) = (position, &text.position) {
+                let len_text_value = text.value.len();
+                let pos_start = Position {
+                    line: (link_ref_pos.start.line - 1) as u32,
+                    character: (link_ref_pos.start.column + len_text_value + 2) as u32,
+                };
+                let pos_end = Position {
+                    line: (link_ref_pos.end.line - 1) as u32,
+                    character: (link_ref_pos.end.column - 2) as u32,
+                };
+                return Some(PrepareRenameResponse::Range(Range {
+                    start: pos_start,
+                    end: pos_end,
+                }));
+            }
+        }
+        Node::Definition(Definition { position, .. }) => {
+            log::info!("RENAME NODE: {:?}", node);
+            return Some(PrepareRenameResponse::DefaultBehavior {
+                default_behavior: true,
+            });
+        }
+        Node::FootnoteReference(FootnoteReference { position, .. }) => {
+            log::info!("RENAME NODE: {:?}", node);
+            return Some(PrepareRenameResponse::DefaultBehavior {
+                default_behavior: true,
+            });
+        }
+        Node::FootnoteDefinition(FootnoteDefinition { position, .. }) => {
             log::info!("RENAME NODE: {:?}", node);
             return Some(PrepareRenameResponse::DefaultBehavior {
                 default_behavior: true,
@@ -44,4 +81,13 @@ pub fn find_renameable_for_position<'a>(node: &'a Node, req_pos: &Position) -> O
     }
 
     traverse_ast!(node, find_renameable_for_position, req_pos)
+}
+
+fn get_link_ref_text(children: &Vec<Node>) -> Option<&Text> {
+    for child in children {
+        if let Node::Text(t) = child {
+            return Some(t);
+        }
+    }
+    None
 }
